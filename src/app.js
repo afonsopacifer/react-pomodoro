b30329e224fb5efa02f34aec3962d80c608669ed
afonsopacifer <afonsopolis@gmail.com>
2016-04-27 21:48:11 -0300

    <Timer/> Created

--- src/app.js
+++ src/app.js
@@ -1,12 +1,33 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-class Layout extends React.Component {
+
+// <Timer />
+// ----------------------------------
+const Timer = React.createClass({
+
+ getInitialState () {
+    return {times: 500};
+  },
+
+  componentDidMount() {
+    setInterval(this.run, 1000);
+  },
+
+  run() {
+    let oldstate = this.state.times;
+    let newstate = oldstate - 1;
+    this.setState({times: newstate});
+  },
+
   render() {
     return (
-      <h1>Hello</h1>
-    );
+      <div>
+        {this.state.times}
+      </div>
+    )
   }
-}
 
-ReactDom.render(<Layout/>, document.getElementById('app'));
+});
+
+ReactDom.render(<Timer/>, document.getElementById('app'));
