2a6249eac688663c4635859e47931685550132d2
afonsopacifer <afonsopolis@gmail.com>
2016-04-28 01:37:58 -0300

    Layout component <Pomodoro/> created

--- src/app.js
+++ src/app.js
@@ -1,33 +1,61 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-
 // <Timer />
 // ----------------------------------
 const Timer = React.createClass({
 
- getInitialState () {
-    return {times: 500};
+  getInitialState () {
+    return {time: this.props.time};
   },
 
   componentDidMount() {
-    setInterval(this.run, 1000);
+    setInterval(this.elapse, 1000);
   },
 
-  run() {
-    let oldstate = this.state.times;
-    let newstate = oldstate - 1;
-    this.setState({times: newstate});
+  elapse() {
+    let oldState = this.state.time;
+    let newState = oldState - 1;
+    this.setState({time: newState});
+  },
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({time: nextProps.time});
   },
 
   render() {
     return (
       <div>
-        {this.state.times}
+        {this.state.time}
       </div>
     )
   }
 
 });
 
-ReactDom.render(<Timer/>, document.getElementById('app'));
+// <Pomodoro />
+// ----------------------------------
+const Pomodoro = React.createClass({
+
+ getInitialState () {
+    return {time: 100};
+  },
+
+  setTime(newTime) {
+    this.setState({time: newTime});
+  },
+
+  render() {
+    return (
+      <div>
+        <Timer time={this.state.time}/>
+        <button onClick={this.setTime.bind(this, 1000)}>Pomodoro</button>
+        <button onClick={this.setTime.bind(this, 500)}>Short</button>
+        <button onClick={this.setTime.bind(this, 2000)}>Long</button>
+      </div>
+    )
+  }
+
+});
+
+ReactDom.render(<Pomodoro/>, document.getElementById('app'));
