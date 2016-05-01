c0b91b251bb40b4c848196a3e5152e2173c2c1c8
afonsopacifer <afonsopolis@gmail.com>
2016-05-01 00:03:10 -0300

    folders architecture refactor

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var path = require('path');
 module.exports = {
   context: path.join(__dirname, "src"),
   devtool: debug ? "inline-sourcemap" : null,
-  entry: "./app.js",
+  entry: "./js/app.js",
   module: {
     loaders: [
       {
