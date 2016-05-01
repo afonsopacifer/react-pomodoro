64d140bbaa4a7ae5850b4fea231ba767db8d953c
Mauricio Junior <mjuniior92@gmail.com>
2016-05-01 19:55:23 -0300

    fix bug play

--- src/js/app.js
+++ src/js/app.js
@@ -44,6 +44,7 @@ const Pomodoro = React.createClass({
   },
 
   play() {
+    if(this.state.play) { return false; }
     clearInterval(this.interval);
     this.interval = setInterval(this.elapseTime, 1000);
     this.setState({play: true});
