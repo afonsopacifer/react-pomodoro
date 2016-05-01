ab77da46617c5997bfc09423e9f0688aa590b07b
Willian Justen <willianjustenqui@gmail.com>
2016-05-01 14:29:23 -0300

    New footer component

---
+++ src/js/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = React.createClass({
+  render() {
+    return (
+      <footer className="credits">
+        Made with <span className="heart"></span> by <a href="https://github.com/afonsopacifer" className="link" target="_blank">@afonsopacifer</a>
+      </footer>
+    )
+  }
+});
+
+export default Footer;
