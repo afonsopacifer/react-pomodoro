c908837a569c853665eb2a516d10f1992376788e
Matheus Kautzmann <kautzmann5@gmail.com>
2016-05-02 16:27:36 -0300

    Change to ES6 class and provide early bindings

--- src/js/app.js
+++ src/js/app.js
@@ -3,20 +3,28 @@ import ReactDom from "react-dom";
 import Footer from "./footer.js";
 import GithubCorner from "react-github-corner";
 
-const Pomodoro = React.createClass({
+class Pomodoro extends React.Component {
 
-  getInitialState () {
-    return {
+  constructor() {
+    super();
+    this.state = {
       time: 0,
       play: false,
       timeType: 0
     };
-  },
+    // Bind early, avoid function creation on render loop
+    this.setTimeForCode = this.setTime.bind(this, 1500);
+    this.setTimeForSocial = this.setTime.bind(this, 300);
+    this.setTimeForCoffee = this.setTime.bind(this, 900);
+    this.reset = this.reset.bind(this);
+    this.play = this.play.bind(this);
+    this.elapseTime = this.elapseTime.bind(this);
+  }
 
   componentDidMount() {
     this.setTime(1500);
     Notification.requestPermission();
-  },
+  }
 
   elapseTime() {
     if (this.state.time === 0) {
@@ -27,14 +35,14 @@ const Pomodoro = React.createClass({
       let newState = this.state.time - 1;
       this.setState({time: newState});
     }
-  },
+  }
 
   format(seconds) {
     let m = Math.floor(seconds % 3600 / 60);
     let s = Math.floor(seconds % 3600 % 60);
     let timeFormated = ((m < 10 ? "0" : "") : "") + m + ":" + (s < 10 ? "0" : "") + s;
     return timeFormated;
-  },
+  }
 
   formatType(timeType) {
     let timeTypeFormated;
@@ -42,25 +50,25 @@ const Pomodoro = React.createClass({
     if(timeType === 300){timeTypeFormated = "social";}
     if(timeType === 900){timeTypeFormated = "coffee";}
     return timeTypeFormated;
-  },
+  }
 
   play() {
     if(this.state.play) { return false; }
     clearInterval(this.interval);
     this.interval = setInterval(this.elapseTime, 1000);
     this.setState({play: true});
-  },
+  }
 
-  reset(resetFor) {
+  reset(resetFor = this.state.time) {
     clearInterval(this.interval);
     let time = this.format(resetFor);
     this.setState({play: false});
-  },
+  }
 
   setTime(newTime) {
     this.reset(newTime);
     this.setState({time: newTime, timeType: newTime});
-  },
+  }
 
   alert() {
     // vibration
@@ -89,7 +97,7 @@ const Pomodoro = React.createClass({
         });
       }
     }
-  },
+  }
 
   render() {
     return (
@@ -110,9 +118,9 @@ const Pomodoro = React.createClass({
           </div>
 
           <div className="container">
-            <button className="btn" onClick={this.setTime.bind(this, 1500)}>Code</button>
-            <button className="btn" onClick={this.setTime.bind(this, 300)}>Social</button>
-            <button className="btn" onClick={this.setTime.bind(this, 900)}>Coffee</button>
+            <button className="btn" onClick={this.setTimeForCode}>Code</button>
+            <button className="btn" onClick={this.setTimeForSocial}>Social</button>
+            <button className="btn" onClick={this.setTimeForCoffee}>Coffee</button>
           </div>
 
         </div> {/* main */}
@@ -126,7 +134,7 @@ const Pomodoro = React.createClass({
 
               <div className="controlsPlay">
                 <button className="play btnIcon" onClick={this.play}></button>
-                <button className="stop btnIcon" onClick={this.reset.bind(this, this.state.time)}></button>
+                <button className="stop btnIcon" onClick={this.reset}></button>
               </div>
 
               <div className="controlsCheck">
@@ -161,7 +169,6 @@ const Pomodoro = React.createClass({
       </div> /* bottomBar */
     )
   }
-
-});
+}
 
 ReactDom.render(<Pomodoro/>, document.getElementById('app'));
