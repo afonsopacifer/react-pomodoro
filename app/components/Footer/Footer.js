1d430c4e466fa161f71e900520cc46462a4bde3a
Caue Alves <cauealveseng@gmail.com>
2016-06-06 03:01:15 -0300

    Add default configuration for make tests

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -1,9 +1,13 @@
+'use strict';
+
 import React from 'react';
 
-export default function Footer() {
-  return (
-    <footer className="credits">
-      Made with <span className="heart"></span> by <a href="https://github.com/afonsopacifer" className="link" target="_blank">@afonsopacifer</a>
-    </footer>
-  );
-}
+export default class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="credits">
+        Made with <span className="heart"></span> by <a href="https://github.com/afonsopacifer" className="link" target="_blank">@afonsopacifer</a>
+      </footer>
+    );
+  };
+};
