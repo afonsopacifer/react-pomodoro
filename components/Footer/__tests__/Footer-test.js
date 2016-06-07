731ffb3d2bcdeebe404d76e40a942767c8062a6b
Cauê Alves <cauealveseng@gmail.com>
2016-06-07 20:07:27 -0300

    Use mocha + jsdom instead of jest

--- components/Footer/__tests__/Footer-test.js
+++ components/Footer/__tests__/Footer-test.js
@@ -1,35 +1,29 @@
 'use strict';
 
-jest.unmock('./../Footer');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import Footer from './../Footer';
 
+import { assert } from 'chai';
+
 describe('Footer', () => {
 
+  const footer = TestUtils.renderIntoDocument(
+    <Footer />
+  );
+
   it('verify the text content are correctly', () => {
-
-    const footer = TestUtils.renderIntoDocument(
-      <Footer />
-    );
-
-    const footerNode = ReactDOM.findDOMNode(footer);
-
-    expect(footerNode.textContent).toEqual('Made with  by @afonsopacifer');
+    let footerNode = ReactDOM.findDOMNode(footer);
+    
+    assert.equal(footerNode.textContent, 'Made with  by @afonsopacifer');
   });
 
   it('link of github author should be https://github.com/afonsopacifer', () => {
+    let linkElement = TestUtils.scryRenderedDOMComponentsWithTag(footer, 'a');
 
-    const footer = TestUtils.renderIntoDocument(
-      <Footer />
-    );
-
-    const linkElement = TestUtils.scryRenderedDOMComponentsWithTag(footer, 'a');
-
-    expect(linkElement.length).toEqual(1);
-    expect(linkElement[0].getAttribute('href')).toEqual('https://github.com/afonsopacifer');
+    assert.equal(linkElement.length, 1);
+    assert.equal(linkElement[0].getAttribute('href'), 'https://github.com/afonsopacifer', 'the link is incorrect');
   });
-  
+
 });
