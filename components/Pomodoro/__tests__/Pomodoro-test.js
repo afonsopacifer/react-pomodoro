4ee5baf95a656de7c99f5eb4a068358edecbeb80
Cauê Alves <cauealveseng@gmail.com>
2016-06-07 21:25:15 -0300

    Add tests for check the default state are correctly

---
+++ components/Pomodoro/__tests__/Pomodoro-test.js
@@ -0,0 +1,25 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import Pomodoro from './../Pomodoro';
+
+import { assert } from 'chai';
+
+describe('Test Pomodoro Component', () => {
+  var component; 
+
+  beforeEach(function() {
+    component = TestUtils.renderIntoDocument(
+      <Pomodoro />
+    );
+  });
+
+  it('verify the default state are correctly', () => {
+    assert.equal(component.state.time, 1500);
+    assert.equal(component.state.play, true);
+    assert.equal(component.state.timeType, 1500);
+    assert.equal(component.state.title, '25:00 | Pomodoro timer');
+  });
+});
