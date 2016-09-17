9e33b506dbee937b5edf2bf64498e9727e8b3acc
Jessica <jessica.nascimento6@gmail.com>
2016-09-17 17:14:40 -0300

    Created stateless footer component

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -2,12 +2,10 @@
 
 import React from 'react';
 
-export default class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="credits">
-        Made with <span className="heart"></span> by <a href="https://github.com/afonsopacifer" className="link" target="_blank">@afonsopacifer</a>
-      </footer>
-    );
-  };
-};
+const Footer = () => (
+  <footer className="credits">
+    Made with <span className="heart"></span> by <a href="https://github.com/afonsopacifer" className="link" target="_blank">@afonsopacifer</a>
+  </footer>
+);
+
+export default Footer;
